b9985f0dbf80454d94224dc595a3cfbdf40d7829
Daniel Boros <dancixx@gmail.com>
2022-12-26 16:04:30 +0100

    last chages

--- main.cpp
+++ main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
-#include "pointers.cpp"
 
 int main()
 {
-    pointers();
+    int int_data{33};
+    double double_data{55};
+
+    // refs
+    int &ref_int_data{int_data};
+    double &ref_double_data{double_data};
+
+    // print
+    std::cout << "int_data: " << int_data << std::endl;
+    std::cout << "int_data: " << &int_data << std::endl;
+    std::cout << "double_data: " << double_data << std::endl;
+    std::cout << "double_data: " << &double_data << std::endl;
+
+    std::cout << "int_data: " << ref_int_data << std::endl;
+    std::cout << "int_data: " << &ref_int_data << std::endl;
+    std::cout << "double_data: " << ref_double_data << std::endl;
+    std::cout << "double_data: " << &ref_double_data << std::endl;
+
+    int_data = 111;
+    double_data = 67.2;
+
+    std::cout << "int_data: " << int_data << std::endl;
+    std::cout << "int_data: " << &int_data << std::endl;
+    std::cout << "double_data: " << double_data << std::endl;
+    std::cout << "double_data: " << &double_data << std::endl;
+
+    std::cout << "int_data: " << ref_int_data << std::endl;
+    std::cout << "int_data: " << &ref_int_data << std::endl;
+    std::cout << "double_data: " << ref_double_data << std::endl;
+    std::cout << "double_data: " << &ref_double_data << std::endl;
+
+    ref_int_data = 1011;
+    ref_double_data = 2200.12;
+
+    std::cout << "int_data: " << ref_int_data << std::endl;
+    std::cout << "int_data: " << &ref_int_data << std::endl;
+    std::cout << "double_data: " << ref_double_data << std::endl;
+    std::cout << "double_data: " << &ref_double_data << std::endl;
+
+    // compare refs and pointers
+
     // message to operating system that the main executed
     return 0;
 }
